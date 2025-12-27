2930498dfd7a4cb629e143c46064355f64a4bcc3
Youness Ben <tudorbenhadda@gmail.com>
2025-12-27 23:20:28 +0100

    Finishing the sidebar

---
+++ src/Component/Icons.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HomeIcon , ChartPieIcon, CurrencyEuroIcon } from '@heroicons/react/24/outline'
+
+export const DashBoardIcon = () => <HomeIcon className="" />
+
+export const ExpensesIcon = () => <CurrencyEuroIcon className="" />
+
+export const StatsIcon = () => <ChartPieIcon className="" />
+
+export default Icons
--- src/Component/SideBar.jsx
+++ src/Component/SideBar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import { ChartPieIcon, CurrencyEuroIcon, HomeIcon } from "@heroicons/react/24/outline";
+
 
 const Sidebar = () => {
 
     return (
-        <nav className="bg-black h-screen w-64 flex flex-col">
-            <h1 className="m-b-8 text-white">Expenses Tracker</h1>
-            <div className="text-white flex flex-col items-start space-y-4">
-                <Link to ='/Dash'>DashBoard</Link>
-                <Link to ='/Exp'>Expenses</Link>
-                <Link to ='/Stat'>Stats</Link>
+        <nav style={{backgroundColor : '#2F0000'}} className="h-screen w-64 flex flex-col">
+            <h1 className="m-4 mb-8 text-white">Expenses Tracker</h1>
+            <div className="text-white flex flex-col items-start space-y-2 ml-4">
+                <Link to ='/Dash' className="flex hover:text-[#EFDFBB]"> <HomeIcon className="h-4 mr-2 mt-1"/> DashBoard</Link>
+                <Link to ='/Exp' className="flex hover:text-[#EFDFBB]"> <CurrencyEuroIcon className="h-4 mr-2 mt-1"/> Expenses</Link>
+                <Link to ='/Stat' className="flex hover:text-[#EFDFBB]"> <ChartPieIcon className="h-4 mr-2 mt-1" /> Stats</Link>
             </div>
         </nav>
     );
@@ -17,3 +19,4 @@ const Sidebar = () => {
 }
 
 export default Sidebar;
+
